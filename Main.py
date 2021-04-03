97d4f4600f89363657144dc2ed63d2c9958f4ce6
Ciprian Silivestru <csilivestru@ufl.edu>
2021-04-03 13:37:19 -0400

    Update: Renamed QML class ScanModel to QML_***

--- Main.py
+++ Main.py
@@ -4,8 +4,7 @@ from PyQt5.QtQuick import QQuickView
 from PyQt5.QtQml import QQmlEngine, QQmlComponent, QQmlApplicationEngine, QQmlContext
 
 # Classes
-from model.classes.ScanModel import ScanModel
-from model.classes.ImageModel import ImageModel
+from model.classes.QML_ScanModel import ScanModel
 
 import sys
 
