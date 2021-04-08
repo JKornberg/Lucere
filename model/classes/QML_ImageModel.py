01a3cb534e2f362df38f066ddbd6ee961ed98f64
Ciprian Silivestru <csilivestru@ufl.edu>
2021-04-08 16:57:26 -0400

    In progress: QMl Image Model...

--- model/classes/QML_ImageModel.py
+++ model/classes/QML_ImageModel.py
@@ -1,4 +1,11 @@
 from PyQt5.QtCore import QAbstractListModel, QModelIndex, Qt, QVariant, QUrl
+from ZODB import FileStorage, DB
+import ZODB.config
+import os
+import sys
+from model.classes.Trial import Trial
+from datetime import datetime
+import math
 
 class ImageModel(QAbstractListModel):
 
@@ -9,7 +16,23 @@ class ImageModel(QAbstractListModel):
     def __init__(self, parent=None):
         super(ImageModel, self).__init__(parent)
 
+        # Append classes folder to PATH
+        sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+
+        # Open database
+        db = ZODB.config.databaseFromURL(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'z.conf'))
+        connection = db.open()
+        root = connection.root()
+
         self.imageList = []
+        for index in root.trials:
+            trial = root.trials[index]
+            captures = {
+                # Add images here - 2d array??
+            }
+            self.scanList.append(captures)
+
+        connection.close()
 
     def add(self, item):
         self.beginInsertRows(QModelIndex(), self.rowCount(), self.rowCount())
