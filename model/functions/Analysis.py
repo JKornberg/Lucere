d3b5f6949822645e98098251f1342d2f09fb3bc2
Jonah Kornberg <kornberg.jonah@gmail.com>
2021-04-18 15:59:49 -0400

    Add file demonstrating analysis

--- model/functions/Analysis.py
+++ model/functions/Analysis.py
@@ -2,19 +2,20 @@ import numpy as np
 from skimage import data
 import skimage
 from skimage import io
-class Analysis:
-    def analyze(self, path):
-        image = io.imread(path)
-        value = self.getDetection(image)
-        detected = value > 5 #Replace with threshhold based on data
-        wavelength = self.getWavelength(image)
-        return (detected, wavelength, value)
 
-    def getDetection(image):
-        averages = (image[:,:,0].astype('float64') + image[:,:,1].astype('float64') ) /2 #Remove blue channel
-        flat = averages.flatten()
-        flat = np.ma.masked_equal(flat,0).comporessed()
-        return np.average(flat)
+def getDetection(image):
+    averages = (image[:,:,0].astype('float64') + image[:,:,1].astype('float64') ) /2 #Remove blue channel
+    flat = averages.flatten()
+    flat = np.ma.masked_equal(flat,0).compressed()
+    return np.average(flat)
 
-    def getWavelength(image):
-        return 600
+def getWavelength(image):
+    return 600
+
+def analyze(path):
+    image = io.imread(path)
+    value = getDetection(image)
+    detected = value > 5 #Replace with threshhold based on data
+    wavelength = getWavelength(image)
+    return (detected, wavelength, value)
+
---
+++ model/functions/Test.py
@@ -0,0 +1,3 @@
+from Analysis import *
+x = analyze('D:/School/Senior Project/py/samples/TestScan_1.png')
+print(x)
