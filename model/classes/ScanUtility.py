8701ab03cfa67963fda5707a47a38e3fa3617140
Jonah Kornberg <kornberg.jonah@gmail.com>
2021-04-20 17:22:25 -0400

    update analysis algorithms, add to ScanUtility.py

---
+++ model/classes/ScanUtility.py
@@ -0,0 +1,125 @@
+import numpy as np
+from skimage import data
+import skimage
+from skimage import io
+import math
+from most_common_color import mostCommonRGBfrom
+import Result
+
+class ScanUtility:
+    def getDetection(self,image):
+        averages = (image[:,:,0].astype('float64') + image[:,:,1].astype('float64') ) /2 #Remove blue channel
+        flat = averages.flatten()
+        flat = np.ma.masked_equal(flat,0).compressed()
+        return np.average(flat)
+
+    def getWavelength(self,image):
+        print("Starting Wavelength analysis")
+        # Populate dictionary with all wavelenghts and RGB coresponding values
+        waveArray = []
+        for i in range (510, 610):
+            waveArray.append(self.nmToRGB(i))
+        waveArray = np.array(waveArray)
+        # Get RGB from image
+        RGB = mostCommonRGBfrom(image)
+        wl =  self.rgbToNm(RGB[0], RGB[1], RGB[2], waveArray)
+        return wl
+
+    def analyze(self,path):
+        image = io.imread(path)
+        value = self.getDetection(image)
+        detected = value > 5 #Replace with threshhold based on data
+        wavelength = self.getWavelength(image)
+        return Result(detected, wavelength, value)
+
+
+
+    def nmToRGB(self,wavelength):
+        gamma = 0.80
+        intensityMax = 255
+        factor = 0
+        red = 0
+        green = 0
+        blue = 0
+
+        if(wavelength >= 380 and wavelength<440):
+            red = -(wavelength - 440) / (440 - 380)
+            green = 0.0
+            blue = 1.0
+        elif(wavelength >= 440 and wavelength<490):
+            red = 0.0
+            green = (wavelength - 440) / (490 - 440)
+            blue = 1.0
+        elif(wavelength >= 490 and wavelength<510):
+            red = 0.0
+            green = 1.0
+            blue = -(wavelength - 510) / (510 - 490)
+        elif(wavelength >= 510 and wavelength<580):
+            red = (wavelength - 510) / (580 - 510)
+            green = 1.0
+            blue = 0.0
+        elif(wavelength >= 580 and wavelength<645):
+            red = 1.0
+            green = -(wavelength - 645) / (645 - 580)
+            blue = 0.0
+        elif(wavelength >= 645 and wavelength<781):
+            red = 1.0
+            green = 0.0
+            blue = 0.0
+        else:
+            red = 0.0
+            green = 0.0
+            blue = 0.0
+        
+        if(wavelength >= 380 and wavelength<420):
+            factor = 0.3 + 0.7*(wavelength - 380) / (420 - 380)
+        elif(wavelength >= 420 and wavelength<701):
+            factor = 1.0
+        elif(wavelength >= 701 and wavelength<781):
+            factor = 0.3 + 0.7*(780 - wavelength) / (780 - 700)
+        else:
+            factor = 0.0
+
+        if (red != 0):
+            red = round(intensityMax * pow(red * factor, gamma))
+        if (green != 0):
+            green = round(intensityMax * pow(green * factor, gamma))
+        if (blue != 0):
+            blue = round(intensityMax * pow(blue * factor, gamma))
+        
+        waveDict = {
+            "wavelength": wavelength,
+            "R": red,
+            "G": green,
+            "B": blue
+        }
+
+        return waveDict
+
+    def getClosestValue(self, value, color, waveArray):
+        return waveArray[min(range(len(waveArray)), key = lambda i: abs(waveArray[i][color]-value))][color]
+
+    def rgbToNm(self, R, G, B, waveArray):
+        # We expect images with wavelength in the range 550-570 so we will account
+        # for range 510-610 where all values of B = 0
+        
+        # Create list with where B == 0
+        waveArrayRG = []
+        for index in range(len(waveArray)):
+            waveArray[index]["B"] = 0
+            waveArrayRG.append(waveArray[index])
+
+        # Check for out of range
+        if(abs(G-R) >= 30 and (G-R) < 0):
+            return -1
+
+        # Get closest value for R
+        RR = self.getClosestValue(R, "R", waveArrayRG)
+
+        # Return wave aproximation
+        for index in range (len(waveArrayRG)):
+            if(waveArrayRG[index]["R"] == RR):
+                return waveArrayRG[index]["wavelength"]
+
+            
+
---
+++ model/classes/most_common_color.py
@@ -0,0 +1,86 @@
+import os
+from PIL import Image
+import numpy as np
+
+
+def mostCommonRGBfrom(image):
+    # Open image and get data
+    
+    img = Image.fromarray(image)
+    width, height = img.size
+
+    # The RGB values we will "snap" to
+    colors = [255, 252, 249, 246, 243, 240, 237, 234, 231, 228, 225, 222, 219, 216, 213, 210, 207, 204, 201, 198, 195, 192, 189, 185, 182, 179, 176, 173, 169, 166, 163, 160, 156, 153, 150, 146, 140, 130, 120, 110, 100, 90, 80, 70, 60, 50, 40, 30, 20, 10, 0]
+
+    original_color_count = {}
+    color_count = {}
+    # Loop through every pixel in the image and modify it
+    for w in np.arange(width):
+        for h in np.arange(height):
+            current_color = img.getpixel((w, h))
+
+            if current_color in original_color_count:
+                original_color_count[current_color] += 1
+            else:
+                original_color_count[current_color] = 1
+
+            r, g, b = current_color
+            r_set = False
+            g_set = False
+            b_set = False
+
+            #  Loop through our allowed values and find the closest value to snap to
+            for i in range(len(colors)):
+                color_one = colors[i]
+                color_two = colors[i + 1]
+
+                if not r_set:
+                    if color_one >= r >= color_two:
+                        distance_one = color_one - r
+                        distance_two = r - color_two
+                        r = color_one if distance_one <= distance_two else color_two
+                        r_set = True
+
+                if not g_set:
+                    if color_one >= g >= color_two:
+                        distance_one = color_one - g
+                        distance_two = g - color_two
+                        g = color_one if distance_one <= distance_two else color_two
+                        g_set = True
+
+                if not b_set:
+                    if color_one >= b >= color_two:
+                        distance_one = color_one - b
+                        distance_two = b - color_two
+                        b = color_one if distance_one <= distance_two else color_two
+                        b_set = True
+
+                if all((r_set, g_set, b_set)):
+                    break
+
+            # Set our new pixel back on the image to see the difference
+            new_rgb = (r, g, b)
+            #img.putpixel((w, h), new_rgb)
+
+            if new_rgb in color_count:
+                color_count[new_rgb] += 1
+            else:
+                color_count[new_rgb] = 1
+
+    # Count and sort the colors
+    all_colors = color_count.items()
+    all_colors = sorted(all_colors, key=lambda tup: tup[1], reverse=True)
+
+    # Remove black, white and gray
+    filtered_colors = [color for color in all_colors if not color[0][0] == color[0][1] == color[0][2]]
+
+    # Remove other blacks
+    filtered_colors = [color for color in all_colors if not color[0][0] == color[0][2]]
+
+
+    # Eliminate all blue colors
+    R = int(filtered_colors[0][0][0])
+    G = int(filtered_colors[0][0][1])
+    B = 0
+
+    return (R, G, B)
